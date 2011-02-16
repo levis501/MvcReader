e21756c2e6c63ebe9dd0c93699fe8b4f95d2b385
Steven Levis <levis501@yahoo.com>
2011-02-16 15:53:28 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MvcReader::Application.config.secret_token = '50edf72c43655e08e6b1882a44bbfbd6632d4f527bc7a092a06531ed13bd683cea3b74a84666a80a0516b500f768f0a3b098c1028393f02ee7af278d2b9168b7'
